a5a4b82504ed724ddc7793f19b79431b6b5cafcc
sadeshwar94 <sadeshwarnakulasooriya@gmail.com>
2021-03-08 10:44:34 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello React</h1>
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    const name = 'Ken the Ninja';
+
+    const imgUrl = 'https://s3-ap-northeast-1.amazonaws.com/progate/shared/images/lesson/react/kentheninja.png';
+
+return(
+<div>
+
+<h1>{name}</h1>
+
+<img src={imgUrl}/>
+
+</div>
+);
+
+
+
+  }
 }
+
+
+export default App;
